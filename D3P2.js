169aa2cfae22ee65c181da6ba0185c8faa2bd412
Sridar Vijayakumar <sridarvijayakumar@gmail.com>
2026-06-01 21:55:30 +0530

    d3p2 added

---
+++ D3P2.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+    let map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let ch of s) {
+        if (ch === '(' || ch === '{' || ch === '[') {
+            stack.push(ch);
+        } else {
+            if (stack.length === 0 || stack.pop() !== map[ch]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
